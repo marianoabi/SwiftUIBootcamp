e1353ce65fd3a9bdceb797fc8a8867da338fae04
Abi <amariano.fw@gmail.com>
2024-05-19 11:27:04 +0800

    Added text module

---
+++ SwiftUIBootcamp/Text/TextModule.swift
@@ -0,0 +1,24 @@
+//
+//  TextModule.swift
+//  SwiftUIBootcamp
+//
+//  Created by Abi M on 5/19/24.
+//
+
+import SwiftUI
+
+struct TextModule: View {
+    var body: some View {
+        Text("Hello, this is my first SwiftUI reposritory that would be uploaded in Github..")
+            .font(.subheadline) // view modifier
+            .foregroundStyle(.cyan)
+            .fontWeight(.light)
+            .multilineTextAlignment(.center)
+            .italic()
+            .underline()
+    }
+}
+
+#Preview {
+    TextModule()
+}
