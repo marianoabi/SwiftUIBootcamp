ba29d225e7e1e46a7650fa01aa5d061573bfd873
Abi <amariano.fw@gmail.com>
2024-05-19 14:56:49 +0800

    Added shape module

---
+++ SwiftUIBootcamp/Shapes/ShapesModule.swift
@@ -0,0 +1,53 @@
+//
+//  ShapesModule.swift
+//  SwiftUIBootcamp
+//
+//  Created by Abi M on 5/19/24.
+//
+
+import SwiftUI
+
+struct CustomShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+        return path
+    }
+}
+
+struct ShapesModule: View {
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 24)
+            .foregroundStyle(.purple)
+            .frame(width: 50, height: 50)
+
+        Rectangle()
+            .stroke(Color.green, lineWidth: 4)
+            .frame(width: 100, height: 100)
+
+        RoundedRectangle(cornerRadius: 25, style: .continuous)
+//            .stroke(Color.orange, lineWidth: 3)
+            .foregroundStyle(.orange)
+            .frame(width: 150, height: 100)
+
+        Ellipse()
+            .stroke(Color.purple, lineWidth: 2)
+            .frame(width: 200, height: 100)
+
+        CustomShape()
+            .stroke(Color.red, lineWidth: 3)
+            .frame(width: 100, height: 100)
+
+        Capsule()
+            .foregroundStyle(.cyan)
+            .frame(width: 150, height: 50)
+    }
+}
+
+#Preview {
+    ShapesModule()
+}
