fc2687897c6d3df47f30a90997abfbddd2beab59
Abi <amariano.fw@gmail.com>
2024-05-19 17:11:29 +0800

    Added image module

---
+++ SwiftUIBootcamp/Images/ImagesModule.swift
@@ -0,0 +1,38 @@
+//
+//  ImagesModule.swift
+//  SwiftUIBootcamp
+//
+//  Created by Abi M on 5/19/24.
+//
+
+import SwiftUI
+
+struct ImagesModule: View {
+    var body: some View {
+        Text("I")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+
+        Image(systemName: "heart.circle.fill")
+            .resizable()
+            .foregroundStyle(.red)
+            .frame(width: 100, height: 100)
+//            .font(.largeTitle)
+//            .imageScale(.large)
+        Text("Jollibee")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundStyle(.orange)
+            .italic()
+        Image("jollibee")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 200, height: 200)
+            .clipShape(.circle)
+
+    }
+}
+
+#Preview {
+    ImagesModule()
+}
